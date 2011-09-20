70c5d95f624486079093d02e1813b0ad8bc1f1a1
korogodin_iv@mail.ru <korogodin_iv@mail.ru@3f98fd88-d0df-ebd0-bcd2-7fe230b4971f>
2011-09-20 06:55:48 +0000

    Все int переведены в int16. Грубая оценка быстродействия целочисленного алгоритма с int16.

--- fft_cpp.h
+++ fft_cpp.h
@@ -1,1 +1,3 @@
 void FFT_probe();
+
+
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 #include "fft_cpp.h"
-bool  FFT(int *Rdat, int *Idat, int N, int LogN, int Ft_Flag);
 
 int main( int argc, const char* argv[] )
 {
